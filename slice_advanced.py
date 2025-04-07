f53465d40cbad287f324fc471cd5e18ebab28365
SantiMAlvarez <smartinezalvarez@mail.austral.edu.ar>
2025-04-07 15:11:40 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,10 +1,11 @@
 def slice_advanced():
     # Código a implementar utilizando input.
 
-    texto = input()
-    txt = texto [5: :2]
-    print (txt)
-    
+    txt = 'Hello, World!()'
+    print (txt [4: :2])
+
+    tex = "12345678910"
+    print (tex [4] + tex [6] + tex [8] + tex [10])
     
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
