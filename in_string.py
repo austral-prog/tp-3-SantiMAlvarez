695ab9094e4548798a5ca7bc0fad99b4a3fc677c
SantiMAlvarez <smartinezalvarez@mail.austral.edu.ar>
2025-04-07 12:44:34 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -6,11 +6,11 @@ print()
 
 nombre = nombre.lower()
 
-print(f"contiene a:{'a' in nombre}")
-print(f"contiene e:{'e' in nombre}")
-print(f"contiene i:{'i' in nombre}")
-print(f"contiene o:{'o' in nombre}")
-print(f"contiene u:{'u' in nombre}")
+print(f"Contiene a: {'a' in nombre}")
+print(f"Contiene e: {'e' in nombre}")
+print(f"Contiene i: {'i' in nombre}")
+print(f"Contiene o: {'o' in nombre}")
+print(f"Contiene u: {'u' in nombre}")
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
